ae9dc204f067bc0fb7c942eecf6ca620e05631bd
Fernando dos Santos <nando4803@gmail.com>
2021-12-07 17:47:56 +0100

    feat: add reviews component

---
+++ src/components/SectionReviews/content.ts
@@ -0,0 +1,28 @@
+const reviews = [
+  {
+    name: 'Carlos Cafala',
+    image: 'reviewer.jpg',
+    description:
+      'Fui aluno do Fernando dos Santos durante 1 mês no curso de Programação Web e gostei muito de ter ele como formador, sempre a dar-me ideias inovadoras e sempre prestativo a disso é que ele é meu co-tutor.'
+  },
+  {
+    name: 'Sara Tuma',
+    image: 'reviewer.jpg',
+    description:
+      'Trabalhamos ou estudamos durante praticamente um mês, pois foi o tempo definido para o curso. Depois de terminar, nos foi exigido um projecto, com o tema a nossa escolha. Então sobre o Professor, ele é um excelente professor, ser humano incrível (mais do que ser um bom professor, devemos sempre nos esforçar para sermos humanos, e isso ele concerteza é), atencioso (está sempre disponível para tirar as dúvidas), paciente (faz com que o aluno aprenda, mais do que fazer, nós devemos é entender), interativo (está sempre a falar e a rir), compreensivo, e o principal é que ele realmente trabalha nessa area, e assim fica mais simples e é uma grande oportunidade para explorar ainda mais os conhecimentos e ajuda-nos a orientarmo-nos, se quizermos realmente seguir esse caminho (Frontend). Então quando ele me disse sobre seu novo projecto, fiquei muito feliz por ele, e super apoio a ideia. Se ainda está em dúvida quanto a esse profissional que é o Fernando, então deve realmente tentar conhece-lo, pois eu garanto que não se vai arrepender. Hoje em dia já não tenho tantos problemas com FrontEnd, como tinha antes do curso, digo que foi uma optima experiência e estou satisfeita com os resultados, é claro que não foi só atraves do curso (tive que estudar individualmente também) mas valeu muito a pena para mim. Abraços!!'
+  },
+  {
+    name: 'Cordeiro Luís',
+    image: 'reviewer.jpg',
+    description:
+      'Olá✌️! Eu sou o Cordeiro Luís, Front-End Developer, os meus primeiros passos nesse universo, foram quando  fiz o curso de Desenvolvimento Web na BUKA, com o Formador “Fernando dos Santos”. Foi uma esperiência muito boa,  pelo menos para mim, porque além de aprender as Tecnólogias fundamentais sobre o mundo WEB, também mudou a minha forma de pensar e passei a estudar mais. Dizem que o mais importante na vida é saber fazer alguma coisa, ter um ofício, e eu GANHEI um, participando na formação de Desenvolvimento Web com o formador Fernando dos Santos,  Hoje Só um FRONT-END DEVELOPER com um ano de esperência...'
+  },
+  {
+    name: 'Ruth Miguel',
+    image: 'reviewer.jpg',
+    description:
+      'Foi uma experiência boa para mim, apesar de estarmos juntos por pouco tempo, mas no fundo o mais importante é a prática. Obrigado!'
+  }
+]
+
+export default reviews
