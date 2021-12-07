320536da51510696160642111c8834f41105c2cd
Fernando dos Santos <nando4803@gmail.com>
2021-12-07 17:48:18 +0100

    feat: add techs component

---
+++ src/components/SectionTech/content.ts
@@ -0,0 +1,32 @@
+const icons = [
+  {
+    name: 'Terminal',
+    image: 'terminal.svg'
+  },
+  {
+    name: 'Git',
+    image: 'git.svg'
+  },
+  {
+    name: 'GitHub',
+    image: 'github.svg'
+  },
+  {
+    name: 'HTML5',
+    image: 'html-5.svg'
+  },
+  {
+    name: 'CSS3',
+    image: 'css3.svg'
+  },
+  {
+    name: 'Sass',
+    image: 'sass.svg'
+  },
+  {
+    name: 'JavaScript',
+    image: 'javascript.svg'
+  }
+]
+
+export default icons
