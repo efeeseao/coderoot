ecc9714694ec2e85269113e086119d9225d2a19c
Fernando dos Santos <nando4803@gmail.com>
2021-12-07 17:47:33 +0100

    feat: add modules component

---
+++ src/components/SectionModules/content.ts
@@ -0,0 +1,40 @@
+const content = [
+  {
+    title: 'Módulo 1',
+    subtitle: 'Introdução e Conceitos do Terminal',
+    description:
+      '<p>Neste módulo, vamos aprender como o que é ser uma pessoa programadora.Como organizar os estudos. <p>Vamos conhecer as tecnologias que seram usadas durante o curso. E, iremos começar falando sobre o terminal.</p></p>'
+  },
+  {
+    title: 'Módulo 2',
+    subtitle: 'Git',
+    description:
+      '<p>Vamos iniciar o módulo 2, falano sobre o Git, que é um controle de versão, muito usado por Pessoas Desenvolvedoras. Vamos conhecer os principais comanos usados no dia-a-dia.</p>'
+  },
+  {
+    title: 'Módulo 3',
+    subtitle: 'GitHub',
+    description:
+      '<p>Github é uma rede social das Pessoas Desenvolvedoras, uma plataforma de armazenamento de códigos, vamos aprender como funciona e como devemos usar no dia-a-dia.</p>'
+  },
+  {
+    title: 'Módulo 4',
+    subtitle: 'HTML5',
+    description:
+      '<p>Aqui iremos ver todos os conceitos relacionados ao HTML, o que é? Se é ou não uma linguagem de programação hahahaha. <p>Vamos ver as boas práticas no uso dessa tecnólogia, e, montar a primeira estrutura.</p></p>'
+  },
+  {
+    title: 'Módulo 5',
+    subtitle: 'CSS3',
+    description:
+      '<p>Iremos aprender sobre o CSS, que no universo Frontend é o famoso, acerta aqui, quebra aí hahahaha. Vamos entrar de cabeça nos conceitos e também nas boas práticas no uso do CSS.</p>'
+  },
+  {
+    title: 'Módulo 6',
+    subtitle: 'JavaScript ES6+',
+    description:
+      '<p>Por fim, vamos explorar o mundo JavaScript, conhecer os conceitos, boas práticas, como usar. E, conhecer algumas das princiapis tecnologias que estão dentro do ecossistema JavaScript</p>'
+  }
+]
+
+export default content
