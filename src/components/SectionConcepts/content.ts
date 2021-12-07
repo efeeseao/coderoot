abf72cccd6d582d9dc595f2e14327d59110f2f76
Fernando dos Santos <nando4803@gmail.com>
2021-12-07 17:46:27 +0100

    feat: add concepts component

---
+++ src/components/SectionConcepts/content.ts
@@ -0,0 +1,15 @@
+const content = [
+  'Boas práticas com HTML',
+  'Boas práticas com CSS',
+  'Boas práticas com JavaScript',
+  'Boas práticas de Acessibilidade',
+  'Boas práicas de SEO',
+  'Performance',
+  'Flexbox',
+  'Grid Layout',
+  'Sass',
+  'ES6+',
+  'Deploy dos projetos'
+]
+
+export default content
---
+++ src/components/SectionConcepts/styles.ts
@@ -0,0 +1,58 @@
+import styled, { css } from 'styled-components'
+import media from 'styled-media-query'
+
+export const Box = styled.section`
+  ${({ theme }) => css`
+    color: ${theme.colors.black};
+    background-color: ${theme.colors.white};
+    padding: ${theme.spacings.small};
+    border-radius: ${theme.border.radius};
+
+    ${media.greaterThan('medium')`
+      padding: ${theme.spacings.large};
+    `}
+  `}
+`
+
+export const List = styled.ul`
+  ${({ theme }) => css`
+    padding: ${theme.spacings.small} 0 0;
+
+    ${media.greaterThan('medium')`
+      padding: ${theme.spacings.small} ${theme.spacings.xsmall} 0;
+      column-count: 2;
+    `}
+
+    ${media.greaterThan('large')`
+      column-count: 3;
+    `}
+  `}
+`
+
+export const Item = styled.li`
+  ${({ theme }) => css`
+    position: relative;
+    list-style: none;
+    padding-left: ${theme.spacings.small};
+
+    &::after {
+      content: ' ';
+      position: absolute;
+      width: 0.9rem;
+      height: 0.9rem;
+      top: 0.9rem;
+      left: 0rem;
+      border-radius: 100%;
+      background: linear-gradient(
+        255.09deg,
+        #ff60c8 -75.17%,
+        #5b1fa6 102.26%,
+        #7e3dce 102.26%
+      );
+    }
+
+    &:not(:last-child) {
+      margin-bottom: ${theme.spacings.xsmall};
+    }
+  `}
+`
